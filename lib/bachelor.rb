01724e71f4534e9f253e500f471910a5941cbf7d
jonathan choi <jisung594@gmail.com>
2018-12-04 04:13:26 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,71 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num,contestants|
+    if season_num == season
+      contestants.each do |hash|
+        if hash["status"] == "Winner"
+          name = hash["name"].split(" ")
+          #binding.pry
+          return name[0] 
+        end
+      end
+    end
+  end
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num,contestants|
+    contestants.each do |hash|
+      if hash["occupation"] == occupation
+        return hash["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  
+  data.each do |season_num,contestants|
+    contestants.each do |hash|
+      if hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  arr = []
+  
+  data.each do |season_num,contestants|
+    contestants.each do |hash|
+      if hash["hometown"] == hometown
+        arr << hash["occupation"]
+      end
+    end
+  end
+  
+  return arr[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  contestants_total = 0
+  
+  data.each do |season_num,contestants|
+    if season_num == season
+      contestants.each do |hash|
+        sum += hash["age"].to_f
+        contestants_total += 1
+        #binding.pry
+      end
+    end
+  end
+  
+  return (sum / contestants_total).round
 end
